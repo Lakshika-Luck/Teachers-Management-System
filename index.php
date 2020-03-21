82f1b0495a6b4d0257c476d5031023ec6a75d9f7
Lakshika <lakshikarasanjali1114@gmail.com>
2020-03-21 22:23:23 +0530

    My Second

---
+++ index.php
@@ -0,0 +1,276 @@
+﻿<?php
+include("config.php");
+if(!isset($_SESSION))
+{
+	session_start();
+}
+if(isset($_SESSION['usertype']))
+{
+	$usertype=$_SESSION['usertype'];
+}
+else
+{
+	$usertype="guest";
+}
+?>
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+    <meta charset="UTF-8" />
+    <title>Teachers Management System </title>
+     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<meta content="" name="description" />
+	<meta content="" name="author" />
+	<!-- GLOBAL STYLES -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="assets/css/theme.css" />
+    <link rel="stylesheet" href="assets/css/MoneAdmin.css" />
+    <link rel="stylesheet" href="assets/plugins/Font-Awesome/css/font-awesome.css" />
+    <!--END GLOBAL STYLES -->
+
+    <!-- PAGE LEVEL STYLES -->
+    <link href="assets/css/layout2.css" rel="stylesheet" />
+       <link href="assets/plugins/flot/examples/examples.css" rel="stylesheet" />
+       <link rel="stylesheet" href="assets/plugins/timeline/timeline.css" />
+       <link href="assets/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+        <link href="assets/css/jquery-ui.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/plugins/uniform/themes/default/css/uniform.default.css" />
+<link rel="stylesheet" href="assets/plugins/inputlimiter/jquery.inputlimiter.1.0.css" />
+<link rel="stylesheet" href="assets/plugins/chosen/chosen.min.css" />
+<link rel="stylesheet" href="assets/plugins/colorpicker/css/colorpicker.css" />
+<link rel="stylesheet" href="assets/plugins/tagsinput/jquery.tagsinput.css" />
+<link rel="stylesheet" href="assets/plugins/daterangepicker/daterangepicker-bs3.css" />
+<link rel="stylesheet" href="assets/plugins/datepicker/css/datepicker.css" />
+<link rel="stylesheet" href="assets/plugins/timepicker/css/bootstrap-timepicker.min.css" />
+<link rel="stylesheet" href="assets/plugins/switch/static/stylesheets/bootstrap-switch.css" />
+     <?php
+	include("additionalpage.php")
+	?>
+</head>
+
+    <!-- END HEAD -->
+
+    <!-- BEGIN BODY -->
+<body class="padTop53 " >
+
+    <!-- MAIN WRAPPER -->
+    <div id="wrap" >
+        
+
+        <!-- HEADER SECTION -->
+        <div id="top">
+
+            <nav class="navbar navbar-inverse navbar-fixed-top " style="padding-top: 10px;">
+                <a data-original-title="Show/Hide Menu" data-placement="bottom" data-tooltip="tooltip" class="accordion-toggle btn btn-primary btn-sm visible-xs" data-toggle="collapse" href="#menu" id="menu-toggle">
+                    <i class="icon-align-justify"></i>
+                </a>
+                <!-- LOGO SECTION -->
+                <header class="navbar-header">
+
+                    <a href="index.php" class="navbar-brand">
+                    Teachers Management System
+                        
+                        </a>
+                </header>
+                <!-- END LOGO SECTION -->
+                <ul class="nav navbar-top-links navbar-right">
+
+                    <!--END ADMIN SETTINGS -->
+                </ul>
+
+            </nav>
+
+        </div>
+        <!-- END HEADER SECTION -->
+
+
+
+        <!-- MENU SECTION -->
+       <div id="left" >
+            <div class="media user-media well-small">
+                
+                <br />
+                <div class="media-body">
+                    <h5 class="media-heading"> Welcome</h5>
+                    <ul class="list-unstyled user-info">
+                        <?php
+						if(isset($_SESSION['usertype']))
+						{
+							if($usertype=="teach")
+							{
+								$sqlloginuser="SELECT Name FROM school_staff WHERE SID='$_SESSION[username]'";
+							}
+							else
+							{
+								$sqlloginuser="SELECT Name FROM office_staff WHERE SID='$_SESSION[username]'";
+							}
+							$resultloginuser=mysqli_query($connection,$sqlloginuser)or die("Error in sql3".mysqli_error($connection));
+							$rowloginuser = mysqli_fetch_assoc($resultloginuser);
+							$loginname=$rowloginuser["Name"];
+						?>
+                        <li>
+                             <a class="btn btn-success btn-xs btn-circle" style="width: 10px;height: 12px;"></a> <?php echo $loginname; ?>
+                           
+                        </li>
+                       <?php
+						}
+					   ?>
+                    </ul>
+                </div>
+                <br />
+            </div>
+
+            <ul id="menu" class="collapse">
+
+                
+                <li class="panel active">
+                    <a href="index.php" >
+                        <i class="icon-table"></i> Home
+	   
+                       
+                    </a>                   
+                </li>
+
+
+
+                <?php
+				if($usertype=="guest")
+				{
+					include("menu.php");
+				}
+				else if($usertype=="zone")
+				{
+					include("zonemenu.php");
+				}
+				else if($usertype=="teach")
+				{
+					include("teachmenu.php");
+				}
+				?>
+
+            </ul>
+
+        </div>
+        <!--END MENU SECTION -->
+
+
+
+        <!--PAGE CONTENT -->
+        <div id="content">
+             
+            <div class="inner" style="min-height: 700px;">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1> Teachers Management System </h1>
+                    </div>
+                </div>
+                  <hr />
+                 <?php
+				 if(isset($_GET["id"]))
+				 {
+					$page=base64_decode($_GET["id"]).".php";
+					include($page);
+				 }
+				 else
+				 {
+					include("home.php"); 
+				 }
+				 ?>
+
+                
+            </div>
+
+        </div>
+        <!--END PAGE CONTENT -->
+
+         <!-- RIGHT STRIP  SECTION -->
+        <div id="right">
+
+            <?php
+			include("right.php");
+			?>
+          
+            
+         
+
+        </div>
+         <!-- END RIGHT STRIP  SECTION -->
+    </div>
+
+    <!--END MAIN WRAPPER -->
+
+    <!-- FOOTER -->
+    <div id="footer">
+        <p>&copy;  Staff Management System &nbsp;<?php echo date("Y"); ?> &nbsp;</p>
+    </div>
+    <!--END FOOTER -->
+	  <!-- GLOBAL SCRIPTS -->
+    <script src="assets/plugins/jquery-2.0.3.min.js"></script>
+     <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/plugins/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <!-- END GLOBAL SCRIPTS -->
+
+    <!-- PAGE LEVEL SCRIPTS -->
+    <script src="assets/plugins/flot/jquery.flot.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.time.js"></script>
+     <script  src="assets/plugins/flot/jquery.flot.stack.js"></script>
+    <script src="assets/js/for_index.js"></script>
+   
+    <!-- END PAGE LEVEL SCRIPTS -->
+
+
+ <!-- GLOBAL SCRIPTS -->
+    <script src="assets/plugins/jquery-2.0.3.min.js"></script>
+     <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/plugins/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <!-- END GLOBAL SCRIPTS -->
+        <!-- PAGE LEVEL SCRIPTS -->
+    <script src="assets/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/plugins/dataTables/dataTables.bootstrap.js"></script>
+     <script>
+         $(document).ready(function () {
+             $('#dataTables-example').dataTable();
+         });
+    </script>
+	<script>
+         $(document).ready(function () {
+             $('#dataTables-example1').dataTable();
+         });
+    </script>
+	<script>
+         $(document).ready(function () {
+             $('#dataTables-example2').dataTable();
+         });
+    </script>
+     <!-- END PAGE LEVEL SCRIPTS -->
+      <!-- PAGE LEVEL SCRIPT-->
+ <script src="assets/js/jquery-ui.min.js"></script>
+ <script src="assets/plugins/uniform/jquery.uniform.min.js"></script>
+<script src="assets/plugins/inputlimiter/jquery.inputlimiter.1.3.1.min.js"></script>
+<script src="assets/plugins/chosen/chosen.jquery.min.js"></script>
+<script src="assets/plugins/colorpicker/js/bootstrap-colorpicker.js"></script>
+<script src="assets/plugins/tagsinput/jquery.tagsinput.min.js"></script>
+<script src="assets/plugins/validVal/js/jquery.validVal.min.js"></script>
+<script src="assets/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="assets/plugins/daterangepicker/moment.min.js"></script>
+<script src="assets/plugins/datepicker/js/bootstrap-datepicker.js"></script>
+<script src="assets/plugins/timepicker/js/bootstrap-timepicker.min.js"></script>
+<script src="assets/plugins/switch/static/js/bootstrap-switch.min.js"></script>
+<script src="assets/plugins/jquery.dualListbox-1.3/jquery.dualListBox-1.3.min.js"></script>
+<script src="assets/plugins/autosize/jquery.autosize.min.js"></script>
+<script src="assets/plugins/jasny/js/bootstrap-inputmask.js"></script>
+       <script src="assets/js/formsInit.js"></script>
+        <script>
+            $(function () { formInit(); });
+        </script>
+
+
+</body>
+
+    <!-- END BODY -->
+</html>
